869b839e67b951ee0014deb163acf2b3842a2f4e
Eugene Cheah <eugeneqin@gmail.com>
2016-12-26 20:20:57 +0800

    JIRA restore script tweak

--- template/jira/restore.sh
+++ template/jira/restore.sh
@@ -3,4 +3,6 @@
 # for - cptactionhank/atlassian-jira:7.0.5
 #------------------------------------------------
 
+chmod -R 0777 .        # Does a permission nuke (resolve common plugin problems)
+
 tar -xvf "${BACKUP_FILENAME}"; # Uncompresses the backup out
