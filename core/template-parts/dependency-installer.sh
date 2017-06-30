46131ac875a3c0c07c24f8b6e8928bcadfc0888f
Eugene Cheah <eugeneqin@gmail.com>
2017-06-30 17:26:13 +0800

    Backup via CLI refinement

--- core/template-parts/dependency-installer.sh
+++ core/template-parts/dependency-installer.sh
@@ -8,8 +8,14 @@ PATH_APK=$(which apk);
 echo ">>> Ensuring dependencies : ${BACKUP_DEPENDS[@]}"
 for DEP in "${BACKUP_DEPENDS[@]}"
 do
+	# Does a special rewrite for checking mysql-client as mysql
+	EXC_DEP="${DEP}";
+	if [ "${EXC_DEP}" == "mysql-client" ] ; then
+		EXC_DEP="mysql";
+	fi;
+	
 	echo ">>> Ensuring dependency is installed : ${DEP}"
-	if [ -z "$(which ${DEP})" ] ; then
+	if [ -z "$(which ${EXC_DEP})" ] ; then
 		# apt-get found, use it
 		if [ -n "$PATH_APTGET" ] ; then
 			apt-get update || true;
