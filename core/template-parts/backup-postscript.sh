e00809cb3e74157fc579a30d5a448f68ab7cc3a7
Eugene Cheah <eugeneqin@gmail.com>
2017-10-15 23:43:55 +0800

    upload command support for S3CLI for file sizes larger then 4.9 GB

--- core/template-parts/backup-postscript.sh
+++ core/template-parts/backup-postscript.sh
@@ -4,23 +4,70 @@
 
 echo "S3Backup - completed backup file process"
 
-echo "S3Backup - starting curl upload";
+echo "S3Backup - starting upload";
 
-# curl working vars
-CURL_DATEVALUE=`date -R`
-CURL_RESOURCEPATH="/${S3_BUCKET}/${S3_FILEPATH}"
-CURL_STRINGTOSIGN="PUT\n\n${BACKUP_FILETYPE}\n${CURL_DATEVALUE}\n${CURL_RESOURCEPATH}"
-CURL_SIGNATURE=`echo -en ${CURL_STRINGTOSIGN} | openssl sha1 -hmac ${S3_SECRET} -binary | base64`
+# UPLOAD using Python toolchain, if file size is larger then 4.9gb
+if [[ $(find "${BACKUP_FILEPATH}" -type f -size +4.9G 2>/dev/null) ]]; then
+	USE_S3CLI=1
+fi
 
-# curl execution
-curl -X PUT -T "${BACKUP_FILEPATH}" \
-	-H "Host: ${S3_BUCKET}.s3.amazonaws.com" \
-	-H "Date: ${CURL_DATEVALUE}" \
-	-H "Content-Type: ${BACKUP_FILETYPE}" \
-	-H "Authorization: AWS ${S3_KEY}:${CURL_SIGNATURE}" \
-	-L https://${S3_BUCKET}.${S3_HOST}/${S3_FILEPATH};
-	
+if [[ -z $USE_S3CLI ]] ; then
+	#
+	# Use large size python multipart upload
+	#
+
+	# Install python pip
+	echo ">>> Ensuring dependency is installed : python-pip"
+	if [ -z "$(which pip)" ] ; then
+		# apt-get found, use it
+		if [ -n "$PATH_APTGET" ] ; then
+			apt-get update || true;
+			apt-get install -y python-pip || true;
+		fi;
+		# yum found, use it
+		if [ -n "$PATH_YUM" ] ; then
+			yum update || true;
+			yum install -y python-pip || true;
+		fi;
+	fi;
+
+	# Upgrade pip
+	pip install --upgrade pip;
+
+	# Install aws CLI
+	pip install awscli --upgrade --user;
+
+	# Get python install path
+	export PATH=~/.local/bin:$PATH;
+
+	# Prepare the aws credentials
+	export AWS_ACCESS_KEY_ID="$S3_KEY"
+	export AWS_SECRET_ACCESS_KEY="$S3_SECRET"
+
+	# Copies the file over
+	aws s3 cp "${BACKUP_FILEPATH}" "s3://${S3_BUCKET}/${S3_FILEPATH}"
+
+else
+	#
+	# Use curl upload
+	#
+
+	# curl working vars
+	CURL_DATEVALUE=`date -R`
+	CURL_RESOURCEPATH="/${S3_BUCKET}/${S3_FILEPATH}"
+	CURL_STRINGTOSIGN="PUT\n\n${BACKUP_FILETYPE}\n${CURL_DATEVALUE}\n${CURL_RESOURCEPATH}"
+	CURL_SIGNATURE=`echo -en ${CURL_STRINGTOSIGN} | openssl sha1 -hmac ${S3_SECRET} -binary | base64`
+
+	# curl execution
+	curl -X PUT -T "${BACKUP_FILEPATH}" \
+		-H "Host: ${S3_BUCKET}.s3.amazonaws.com" \
+		-H "Date: ${CURL_DATEVALUE}" \
+		-H "Content-Type: ${BACKUP_FILETYPE}" \
+		-H "Authorization: AWS ${S3_KEY}:${CURL_SIGNATURE}" \
+		-L https://${S3_BUCKET}.${S3_HOST}/${S3_FILEPATH};
+fi
+
 #-H "x-amz-storage-class: ${S3_CLASS}" \
 #-H "x-amz-tagging: ${S3_TAG}" \
 	
-echo "S3Backup - completed curl upload";
+echo "S3Backup - completed upload";
