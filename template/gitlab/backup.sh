9e303d58acb3862f1fef5b0557cddd4cc8b25f2b
Eugene Cheah <eugeneqin@gmail.com>
2017-10-17 10:50:32 +0800

    Still cant fix gitlab backup command =(

--- template/gitlab/backup.sh
+++ template/gitlab/backup.sh
@@ -6,11 +6,25 @@
 # # Former backup cleanup
 rm -f *.tar;
 
-# Actual backup
-trap 'gitlab-rake gitlab:backup:create' SIGTERM SIGINT SIGFPE SIGSTP EXIT SIGHUP SIGQUIT;
+# Actual backup function
+gitlab-rake gitlab:backup:create 2> /dev/null;
 
+#
 # Wait for completion
-wait < <(jobs -p);
+#
+# Sadly as the above "gitlab:backup" happens "asyncronously"
+# The only way i could manage to get this script to somewhat work
+# is to wait for the final .tar file (assuming it .tar succeds)
+# 
+# The tar file is assumed to be completed, when it has no changes for more then 10 seconds
+#
+while [ ! -n "$(find . -type f -iname "*.tar" -not -newermt '-10 seconds' 2> /dev/null)" ]
+do
+	echo ".";
+	sleep 10;
+done
+
+# Rename backup
 mv *_backup.tar gitlab-backup.tar;
 
 # Something for debugging
